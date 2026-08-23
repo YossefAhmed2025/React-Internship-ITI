a76a26ad72db076db2f10f8e3a327ef5a4d7099f
Youssef Ahmed <yosefahmed00022@gmail.com>
2026-08-24 00:10:48 +0300

    Add Day 11 Task

---
+++ Day_11_Task/js/Activity.js
@@ -0,0 +1,55 @@
+/*Activity 1*/
+/*
+var number = 7;
+
+if (number > 0) {
+    console.log("Number is positive.");
+} else if (number < 0) {
+    console.log("Number is negative.");
+} else {
+    console.log("Number is zero.");
+}
+
+if (number % 2 === 0) {
+    console.log("Number is even.");
+} else {
+    console.log("Number is odd.");
+}*/
+
+
+/* Task 1*/
+/*
+var PlayerOneChoice = 'Rock';
+var PlayerTwoChoice = 'Paper';
+
+if (PlayerOneChoice === PlayerTwoChoice) {
+    console.log("It's a draw || tie");
+} else if (
+    (PlayerOneChoice === 'Rock' && PlayerTwoChoice === 'Scissors') ||
+    (PlayerOneChoice === 'Scissors' && PlayerTwoChoice === 'Paper') ||
+    (PlayerOneChoice === 'Paper' && PlayerTwoChoice === 'Rock')
+) {
+    console.log('Player One wins!');
+} else if (
+    (PlayerTwoChoice === 'Rock' && PlayerOneChoice === 'Scissors') ||
+    (PlayerTwoChoice === 'Scissors' && PlayerOneChoice === 'Paper') ||
+    (PlayerTwoChoice === 'Paper' && PlayerOneChoice === 'Rock')
+) {
+    console.log('Player Two wins!');
+}*/
+
+/*Task 2 */
+
+var grade = 85;
+
+if (grade >= 90) {
+    console.log("Excellent");
+} else if (grade >= 80) {
+    console.log("Good");
+} else if (grade >= 70) {
+    console.log("Average");
+} else if (grade >= 60) {
+    console.log("Pass");
+} else {
+    console.log("Fail");
+}
